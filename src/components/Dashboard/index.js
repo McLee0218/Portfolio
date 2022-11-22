c748768561a551fb1088e7095798370ea96913a9
tamekamclean <tamekasmclean@gmail.com>
2022-11-21 23:43:14 -0500

    added links to gthub & linkedin

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -7,11 +7,31 @@ import Mona from '../../Images/mona-the-rivetertocat.png';
 function Dashboard() {
     return (
         <div className='dashboardWrapper'>
-            <GitHubCard />
-            <GitHubCard />
-            <GitHubCard />
-            <GitHubCard />
-            <GitHubCard />
+            <GitHubCard 
+                img={Mona} alt='monaOctacat'
+                title="Mclee GitHub Page"
+                description="This has been an amazing but trying three months of learning new things with new people, exploring the world of tech and getting ready for an illustrious career as a developer. Preview my gitHub to see what ive been working on"
+            />
+            <ResumeCard 
+                 img={Mona} alt='monaOctacat'
+                title="Resume"
+                description="This Resume shows my prior work experience and lets you know why im the perfect fit for your company."
+            />
+            <LinkedinCard
+                 img={Mona} alt='monaOctacat'
+                title="Linked-in"
+                description="Explore my Linked-in Page"
+            />
+            <SpecialProjectCard
+                 img={Mona} alt='monaOctacat'
+                title="Special Projects"
+                description="Here are some special projects i've had the pleasure of working on"
+            />
+            <ContactCard 
+                 img={Mona} alt='monaOctacat'
+                title="Contact Me"
+                description="Send me your info, so we can keep in touch! "
+            />
 
         </div>
     )
@@ -22,68 +42,64 @@ function GitHubCard(props) {
         <section>
         <div id="dashCard" className="dashCard">
             <div className="dashCard_body">
-            <button>
-                <img src={Mona} alt='monaOctacat' style={{ height:200, height:200}}/>
-                    <h2 className='dashCard_title'></h2>
-                    <p className='dashCard_description'>This has been an amazing but trying three months of learning new things with new people, exploring the world of tech and getting ready for an illustrious career as a developer. Preview my gitHub to see what ive been working on</p>
-                    </button>
+                <img src={props.img}/>
+                    <h2 className='dashCard_title'>{props.title}</h2>
+                    <p className='dashCard_description'>{props.description}</p>
+                    <a href='https://github.com/McLee0218' className="dashCard_btn">My GitHub</a>
             </div>
         </div>
         </section>
     )
 }
 
-function ResumeCard() {
+function ResumeCard(props) {
     return (
         <div className="dashCard1">
             <div className="dashCard_body1">
-            <button>
-                <img style={{width: fit}} src={Mona} alt='mona-the-rivertcat.png'/>
-                    <h2 className='dashCard_title1'></h2>
-                    <p className='dashCard_description1'>This has been an amazing but trying three months of learning new things with new people, exploring the world of tech and getting ready for an illustrious career as a developer. Preview my gitHub to see what ive been working on</p>
-                    </button>
+                <img src={props.img}/>
+                    <h2 className='dashCard_title1'>{props.title}</h2>
+                    <p className='dashCard_description1'>{props.description}</p>
+                    <button className="dashCard_btn">Resume</button>
             </div>
         </div>
     )
 }
 
-function LinkedinCard() {
+function LinkedinCard(props) {
     return (
         <div className="dashCard2">
             <div className="dashCard_body2">
-            <button>
-                <img style={{width: "30%"}} src={Mona} alt='mona-the-rivertcat.png'/>
-                    <h2 className='dashCard_title2'></h2>
-                    <p className='dashCard_description2'>This has been an amazing but trying three months of learning new things with new people, exploring the world of tech and getting ready for an illustrious career as a developer. Preview my gitHub to see what ive been working on</p>
-                    </button>
+                <img src={props.img}/>
+                    <h2 className='dashCard_title2'>{props.title}</h2>
+                    <p className='dashCard_description2'>{props.description}</p>
+                    <a href='https://www.linkedin.com/in/tameka-mclean-37639b58/' className="dashCard_btn">Linked-In</a>
             </div>
         </div>
     )
 }
 
-function SpecialProjectCard() {
+function SpecialProjectCard(props) {
     return (
         <div className="dashCard3">
             <div className="dashCard_body3">
-            <button>
-                <img style={{width: "30%"}} src={Mona} alt='mona-the-rivertcat.png'/>
-                    <h2 className='dashCard_title3'></h2>
-                    <p className='dashCard_description3'>This has been an amazing but trying three months of learning new things with new people, exploring the world of tech and getting ready for an illustrious career as a developer. Preview my gitHub to see what ive been working on</p>
-                    </button>
+                <img src={props.img}/>
+                    <h2 className='dashCard_title3'>{props.title}</h2>
+                    <p className='dashCard_description3'>{props.description}</p>
+                    <button className="dashCard_btn">First Special Project</button>
+                    <button className="dashCard_btn1">Second Special Project</button>
             </div>
         </div>
     )
 }
 
-function ContactCard() {
+function ContactCard(props) {
     return (
         <div className="dashCard4">
             <div className="dashCard_body4">
-            <button>
-                <img style={{width: "30%"}} src={Mona} alt='mona-the-rivertcat.png'/>
-                    <h2 className='dashCard_title4'></h2>
-                    <p className='dashCard_description4'>This has been an amazing but trying three months of learning new things with new people, exploring the world of tech and getting ready for an illustrious career as a developer. Preview my gitHub to see what ive been working on</p>
-                    </button>
+                <img src={props.img}/>
+                    <h2 className='dashCard_title4'>{props.title}</h2>
+                    <p className='dashCard_description4'>{props.description}</p>
+                    <button className="dashCard_btn">Contact Me</button>
             </div>
         </div>
     )
