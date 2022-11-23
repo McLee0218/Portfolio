218e8fc71d4348b3389411796235f4cd1c2533fb
tamekamclean <tamekasmclean@gmail.com>
2022-11-23 14:49:00 -0500

    port change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
